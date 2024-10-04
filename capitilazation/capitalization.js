45540712545c32c31b12f3c56236af3d27d5701d
Cecilie <cepe0001@stud.kea.dk>
2024-10-04 11:01:43 +0200

    final commit

---
+++ capitilazation/capitalization.js
@@ -0,0 +1,7 @@
+const aName = "peter";
+
+const capitilazation = aName.substring(2, 3);
+const firstPart = aName.substring(0, 2);
+const lastPart = aName.substring(3);
+
+console.log(firstPart + capitilazation.toUpperCase() + lastPart);
